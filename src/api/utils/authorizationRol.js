f6bc98ddd2ac12093755e18312b02ccd571fdfba
Carolina <angiedylexx@gmail.com>
2023-12-01 02:58:13 -0500

    Nombre del usuario en el banner

--- src/api/utils/authorizationRol.js
+++ src/api/utils/authorizationRol.js
@@ -1,18 +1,18 @@
 function authorizationRol(observerUser, response, idToken, refreshToken) {
   if (observerUser) {
-    if (observerUser.rol === "Soy Docente") {
       return response.status(200).json({
         message:"Welcome teacher",
+        name:observerUser.name,
         idToken, 
         refreshToken
       });
     } else {
       return response.status(200).json({
         message:"Welcome student",
+        name:observerUser.name,
         idToken, 
         refreshToken});
     }
-  }
 }
 
 export default authorizationRol  
